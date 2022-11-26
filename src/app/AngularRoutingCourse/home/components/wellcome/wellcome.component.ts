394a3fdb0f2a9cf454ff2a827703cda589dc5654
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2022-11-26 19:18:57 +0200

    add navbar to main app page

--- src/app/AngularRoutingCourse/home/components/wellcome/wellcome.component.ts
+++ src/app/AngularRoutingCourse/home/components/wellcome/wellcome.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/AngularRoutingCourse/user/service/user.service';
- 
+
 @Component({
   selector: 'app-wellcome',
   templateUrl: './wellcome.component.html',
@@ -8,20 +7,7 @@ import { UserService } from 'src/app/AngularRoutingCourse/user/service/user.serv
 })
 export class WellcomeComponent implements OnInit {
   public pageTitle = 'Welcome';
-  constructor(private userservice: UserService) { }
+  constructor() { }
   ngOnInit(): void {
-  }
-  get isLoggedIn(): boolean {
-    return this.userservice.isLoggedIn;
-  }
-  get userName(): string {
-    if (this.userservice.currentUser) {
-      return this.userservice.currentUser.userName;
-    }
-    return '';
-  }
-  logOut(): void {
-    this.userservice.logout();
-    console.log('Log out');
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from './AngularRoutingCourse/user/service/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularRouteCourse';
+   constructor(private userservice: UserService) { }
+  ngOnInit(): void {
+  }
+  get isLoggedIn(): boolean {
+    return this.userservice.isLoggedIn;
+  }
+  get userName(): string {
+    if (this.userservice.currentUser) {
+      return this.userservice.currentUser.userName;
+    }
+    return '';
+  }
+  logOut(): void {
+    this.userservice.logout();
+    console.log('Log out');
+  }
 }
